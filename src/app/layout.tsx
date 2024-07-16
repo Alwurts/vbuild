5e0c19e8910ed7d4b9843fb02293fc98b36f14f2
Alwurts <jota780@gmail.com>
2024-07-16 22:27:45 +0200

    Add umami tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shadcn Editor",
-  description: "Shadcn Editor",
+	title: "Shadcn Editor",
+	description: "Shadcn Editor",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				{children}
+				<Script
+					defer
+					src="https://umami.alwurts.com/script.js"
+					data-website-id="30e492b4-4593-4c30-828d-dd88e8af6ca9"
+				/>
+			</body>
+		</html>
+	);
 }
